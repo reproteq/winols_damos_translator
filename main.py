4d6d1d4fe35f95f89c419de99eae90c36bd2276b
Alex Gregori Tomás <reproteq@gmail.com>
2020-07-09 12:11:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from googletrans import Translator
+import operator
+
+print(" Winols Damos Translator by Reproteq  2020-07-09 ".center(83, "#"))
+
+
+print("Path to file.a2l")
+path = input(">").replace('/', r'\'')
+
+with open(path, 'r+') as f:
+         
+    translate = Translator().translate       
+    lines = f.readlines()
+    f.seek(0)	
+	
+    for i in range(0, len(lines)):
+        text = lines[i]
+
+        if operator.contains(text, "/"):
+
+            lines[i] = text + '\n'
+
+        if operator.contains(text, '"'):
+
+            text = translate(text, src='de', dest='es').text
+            lines[i] = text + '\n'			
+
+
+        print(lines[i])
+
+    f.writelines(lines)
+
